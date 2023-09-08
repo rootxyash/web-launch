a80a3f2aad3f5cd47169485c60395185f809a149
Yash Devkate <78010388+rootxyash@users.noreply.github.com>
2023-09-08 13:01:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to trigger the rocket launch animation
+document.addEventListener("click", function () {
+    const rocket = document.querySelector(".rocket");
+    rocket.classList.add("launch");
+});
